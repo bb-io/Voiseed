d6a0bff4941468243c29a4205848d870a6b08249
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-07-24 13:16:14 +0300

    remove OnConvertTextToSpeechCompleted_ShouldReturnSuccess test - compilation error

--- Tests.Voiseed/PollingTests.cs
+++ Tests.Voiseed/PollingTests.cs
@@ -6,30 +6,6 @@ namespace Tests.Voiseed
     [TestClass]
     public class PollingTests : TestBase
     {
-
-        [TestMethod]
-        public async Task OnConvertTextToSpeechCompleted_ShouldReturnSuccess()
-        {
-
-            var pollingList = new Apps.Voiseed.Polling.PollingList(InvocationContext);
-            var memory = new DateMemory
-            {
-                //LastInteractionDate = DateTime.UtcNow.AddMinutes(-10),
-                //LastStatus = "PENDING"
-            };
-            var request = new Blackbird.Applications.Sdk.Common.Polling.PollingEventRequest<DateMemory>
-            {
-                Memory = memory
-            };
-            string requestId = "4cc0c8b4-96fc-4bea-8450-87e966284db3";
-            var response = await pollingList.OnConvertTextToSpeechCompleted(request, requestId);
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(response);
-            Console.WriteLine(json);
-
-            Assert.IsNotNull(response);
-            Assert.AreEqual("SUCCESS", response.Memory.LastStatus);
-        }
-
         [TestMethod]
         public async Task OnBatchCompleted_ShouldReturnSuccess()
         {
